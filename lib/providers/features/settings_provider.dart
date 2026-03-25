5d4d9d4845d0a8a1c8e05367e56854c1b2f126ec
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2026-03-25 20:11:48 +0500

    Bump version to v1.21.0 - Stable Release with professional sync

--- lib/providers/features/settings_provider.dart
+++ lib/providers/features/settings_provider.dart
@@ -28,7 +28,7 @@ class SettingsProvider extends ChangeNotifier {
   
   bool isBarcodeScanMode = false;
   bool showProductImages = true;
-  String appVersion = '1.20.3';
+  String appVersion = '1.21.0';
   String? deviceId;
 
   Future<void> loadSettings() async {
