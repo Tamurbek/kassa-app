a817585562267e614189aef0dc58c6832ac00979
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2026-03-27 23:27:30 +0500

    chore: bumped version to 1.22.13

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = 'SimpleSale';
-  static const String appVersion = '1.22.10';
+  static const String appVersion = '1.22.13';
   
   // Storage Keys
   static const String keyIsMaster = 'isMaster';
--- lib/providers/app_state.dart
+++ lib/providers/app_state.dart
@@ -65,7 +65,7 @@ class AppState extends ChangeNotifier {
   bool get isBarcodeScanMode => _isBarcodeScanMode;
   bool _showProductImages = true;
   bool get showProductImages => _showProductImages;
-  String appVersion = '1.13.1';
+  String appVersion = '1.22.13';
 
   double get todaySalesTotal {
     final now = DateTime.now();
