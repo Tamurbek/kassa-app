f22baab3d3b7ddaf9a358402f0338fd9d2cc9a41
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2026-03-28 00:22:51 +0500

    Add Railway notification step to GitHub Actions and bump version to 1.22.17

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = 'SimpleSale';
-  static const String appVersion = '1.22.16';
+  static const String appVersion = '1.22.17';
   
   // Storage Keys
   static const String keyIsMaster = 'isMaster';
--- lib/providers/app_state.dart
+++ lib/providers/app_state.dart
@@ -65,7 +65,7 @@ class AppState extends ChangeNotifier {
   bool get isBarcodeScanMode => _isBarcodeScanMode;
   bool _showProductImages = true;
   bool get showProductImages => _showProductImages;
-  String appVersion = '1.22.16';
+  String appVersion = '1.22.17';
 
   double get todaySalesTotal {
     final now = DateTime.now();
