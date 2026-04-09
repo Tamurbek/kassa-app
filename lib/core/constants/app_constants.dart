7a9d4562749e094b608455ce9cc7f1a57ec24b08
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2026-04-09 09:23:36 +0500

    chore: bump version to 1.22.45

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = 'SimpleSale';
-  static const String appVersion = "1.22.44";
+  static const String appVersion = "1.22.45";
   
   // Storage Keys
   static const String keyIsMaster = 'isMaster';
