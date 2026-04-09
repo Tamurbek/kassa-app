a37fb16b073c980b17fe3adad9accad994dacd6e
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2026-04-09 09:47:24 +0500

    chore: release v1.22.47

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = 'SimpleSale';
-  static const String appVersion = "1.22.46";
+  static const String appVersion = "1.22.47";
   
   // Storage Keys
   static const String keyIsMaster = 'isMaster';
