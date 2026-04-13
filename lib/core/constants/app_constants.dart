6aa9427c884ce87ec39407813f6d339cc3bc16e1
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2026-04-13 17:30:55 +0500

    Final version fix: sync pubspec with constants v1.22.91

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = 'SimpleSale';
-  static const String appVersion = "1.22.90";
+  static const String appVersion = "1.22.91";
   
   // Storage Keys
   static const String keyIsMaster = 'isMaster';
