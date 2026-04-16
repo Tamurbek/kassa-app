3ddf3cca37f6982e0de6aeff40e13a4fb0a97b2b
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2026-04-16 19:19:16 +0500

    fix: display full version with build number

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = 'SimpleSale';
-  static const String appVersion = "1.23.5";
+  static const String appVersion = "1.23.5+102";
   
   // Storage Keys
   static const String keyIsMaster = 'isMaster';
--- lib/providers/features/settings_provider.dart
+++ lib/providers/features/settings_provider.dart
@@ -133,7 +133,7 @@ class SettingsProvider extends ChangeNotifier {
       // 5. Load App Version from platform
       try {
         final packageInfo = await PackageInfo.fromPlatform();
-        appVersion = packageInfo.version;
+        appVersion = '${packageInfo.version}+${packageInfo.buildNumber}';
       } catch (e) {
         debugPrint('Error loading package info: $e');
       }
